1dd8079d734846487281a1b63ef64b34a4c6170c
Juan Yusunguaira <103352879+juanpyusun@users.noreply.github.com>
2023-12-02 16:07:06 -0500

    Update register_page.dart

--- lib/src/register/register_page.dart
+++ lib/src/register/register_page.dart
@@ -10,6 +10,63 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body:Container(
+        width: double.infinity,
+        child: Stack(
+          children: [
+            Positioned(
+              left: -100,
+              top: -80,
+              child: _circleRegister(),
+            ),
+            Positioned(
+              left: 0,
+              top: 55,
+              child: _iconBack(),
+            ),
+            Positioned(
+              left: 35,
+              top: 70,
+              child: _textRegister(),
+            ),
+          ],          
+        ),
+      ),
+    );
+  }
+
+  Widget _textRegister(){
+      return Container(
+        child: Text(
+          'REGISTER',
+          style: TextStyle(
+            color: MyColors.secundaryColor,
+            fontSize: 15,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      );
+  }
+
+  Widget _circleRegister(){
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color: MyColors.primaryColor
+      ),    
+      height: 230,
+      width: 240,
+    );
+  }
+
+  Widget _iconBack(){
+    return IconButton(
+      onPressed: (){},
+      icon: Icon(
+        Icons.arrow_back_ios,
+        color: MyColors.secundaryColor
+      )
+    );
   }
 }
