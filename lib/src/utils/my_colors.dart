3c3a423552b507354930c5a01efa9b7f1bc49447
Juan Yusunguaira <103352879+juanpyusun@users.noreply.github.com>
2023-12-02 15:21:32 -0500

    Nuevo color

--- lib/src/utils/my_colors.dart
+++ lib/src/utils/my_colors.dart
@@ -4,5 +4,6 @@ class MyColors{
   static Color primaryColor = const Color(0xFFE70D32);
   static Color primaryDarkColor = Color(0xFFA7001C);
   static Color primaryOpacityColor = const Color.fromRGBO(231, 13, 50, 0.09);
+  static Color secundaryColor = const Color(0xFFF8F8F8);
 
 }
